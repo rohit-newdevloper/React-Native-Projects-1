789e3111d7d4d3e1b4fe96bd3cbb3c578fa2ea76
David Guben <davidjg6@gmail.com>
2017-07-31 12:15:19 -0400

    adding catch authentication

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
-import {Button, Card, CardSection,Input} from './common';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth.createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({error: 'Authentication failed.' });
+          });
+      });
+  }
 
   render() {
     return (
@@ -26,13 +40,25 @@ class LoginForm extends Component {
         />
       </CardSection>
 
+      <Text style={styles.errorTextStyle}>
+        {this.state.error}
+      </Text>
+
       <CardSection>
-        <Button>
+        <Button onPress={this.onButtonPress}>
           Log in
         </Button>
       </CardSection>
     </Card>
     );
+  }
+};
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 };
 
