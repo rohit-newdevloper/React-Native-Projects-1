779e397e6fabbc3d3aac00ad4f878683b119c078
David Guben <davidjg6@gmail.com>
2017-08-12 13:07:15 -0400

    final touches for action creator

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
   render() {
     const { titleStyle } = styles;
+    console.log(this.props);
+
     return (
       <CardSection>
         <Text style={titleStyle}>
@@ -22,4 +26,5 @@ const styles = {
     paddingLeft: 15
   }
 };
-export default ListItem;
+
+export default connect(null, actions)(ListItem);
