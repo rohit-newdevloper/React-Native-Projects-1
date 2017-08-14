48201166ae944d95cbbb7d234afb425731f052ee
David Guben <davidjg6@gmail.com>
2017-08-14 13:18:30 -0400

    completed form and appt functionality

---
+++ PNPCApptScheduler/src/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ScrollView, Text, Picker } from 'react-native';
+import { Header, Input, CardSection, Card, Button, ScheduleForm } from './components/common';
+
+const App = () => {
+
+  return (
+    <ScrollView style={{ flex: 1 }}>
+      <Header headerText="Book an appointment" />
+      <ScheduleForm />
+      <Button>
+        Submit
+      </Button>
+    </ScrollView>
+  );
+};
+
+
+export default App;
