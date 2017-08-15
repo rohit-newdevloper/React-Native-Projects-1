54895bf81e0aa1494ec4fb4afc78b958246befd4
David Guben <davidjg6@gmail.com>
2017-08-15 12:31:09 -0400

    cleaned up redundancies

--- PNPCApptScheduler/src/app.js
+++ PNPCApptScheduler/src/app.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { ScrollView, Text, Picker } from 'react-native';
-import { Header, CardSection, Card, Button, ScheduleForm } from './components/common';
+import { ScrollView, Image, View, Linking } from 'react-native';
+import { Header, Button, ScheduleForm } from './components/common';
 
 const App = () => {
+  const { imgContainer } = styles;
 
   return (
     <ScrollView style={{ flex: 1 }}>
       <Header headerText="Book an appointment" />
+      <View
+        style={imgContainer}>
+        <Image
+          source={require('./img/BNBLogo.png')}
+        />
+        <Image
+          source={require('./img/PNPCLogo.png')}
+        />
+      </View>
       <ScheduleForm />
-
-      <Button>
+      <Button onPress={() => Linking.openURL('http://mailto:davidjg6@gmail.com')}>
         Submit
       </Button>
     </ScrollView>
   );
 };
 
+const styles = {
+  imgContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 30,
+    paddingRight: 30
+  }
+}
 
 export default App;
