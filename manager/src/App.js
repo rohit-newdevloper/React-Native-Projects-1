86b908a94899c27aca436ec5ddc83b1e16d3544d
David Guben <davidjg6@gmail.com>
2017-08-17 13:59:49 -0400

    adding redux, loginform, common contents and firebase into manager app

---
+++ manager/src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyAyCKIZ_DhOvS5VOGs68EejkTLB62BiGE8",
+      authDomain: "manager-d9ce5.firebaseapp.com",
+      databaseURL: "https://manager-d9ce5.firebaseio.com",
+      projectId: "manager-d9ce5",
+      storageBucket: "",
+      messagingSenderId: "23701733302"
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <LoginForm />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
