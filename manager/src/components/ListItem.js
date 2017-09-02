97395d06a4f90e73b92cb0965da15db03ef77413
David Guben <davidjg6@gmail.com>
2017-09-02 12:16:46 -0400

    added ability to edit users

--- manager/src/components/ListItem.js
+++ manager/src/components/ListItem.js
@@ -5,7 +5,7 @@ import {CardSection} from './common';
 
 class ListItem extends Component {
   onRowPress() {
-    Actions.EmployeeCreate({ employee: this.props.employee });
+    Actions.employeeEdit({ employee: this.props.employee });
   }
 
   render() {
